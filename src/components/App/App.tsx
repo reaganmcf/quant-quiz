10ad886e527a49f733a8d573aa7235e278e568c4
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-02-07 17:15:59 -0500

    [ ADD ] redux reworked

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import { Provider } from 'react-redux'
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux'
+import { IAppState } from '../../store';
+import { IGameState } from '../../store/game_state/types';
 import './App.css';
 
-import { createAppStore } from './../../store'
-
-const appStore = createAppStore()
-
-class App extends React.Component {
-  public render() {
-    return (
-      <Provider store={appStore}>
-        <header>Hello World</header>
-      </Provider>
-    )
-  } 
+type Props = {
+  // gameState: IGameState
 }
 
+export const App: React.FC<Props> = () => {
+  const gameState: IGameState = useSelector(
+    (state: IAppState) => state.gameState 
+  )
 
+  console.log(gameState)
+
+  return (
+    <div className="App">
+      <header>
+        <p>Current Score:{gameState.streak}</p>
+      </header>
+    </div>
+  )
+}
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+
 import App from './components/App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createAppStore } from './store'
+const appStore = createAppStore()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root")
+
+render(
+  <Provider store={appStore}>
+    <App/>
+  </Provider>,
+  rootElement
+)
+
